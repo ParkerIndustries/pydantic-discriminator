bd39d904f37bf224cdeb74c82d034f0837da45a7
Hexa-Hexagon <ehorlukianov@gmail.com>
2024-10-14 17:47:05 +0300

    Removed all manual transitions from type to alias type_ in base_v1.py

--- pydantic_discriminator/base_v1.py
+++ pydantic_discriminator/base_v1.py
@@ -43,14 +43,10 @@ class DiscriminatedBaseModel(
         return other_cls.__new__(other_cls, *args, **kwargs)  # type: ignore
 
     def dict(self, *args, **kwargs) -> dict:
-        super_dict = super().dict(*args, **kwargs)
-        #super_dict[Naming.TYPE_FIELD_ALIAS] = super_dict.pop(Naming.TYPE_FIELD_NAME)
-        return super_dict
+        return super().dict(*args, **kwargs)
 
     @root_validator(pre=True)
     def _validate_type_field(cls, v):
-        if Naming.TYPE_FIELD_NAME in v:
-            v[Naming.TYPE_FIELD_ALIAS] = v.pop(Naming.TYPE_FIELD_NAME)
         if Naming.TYPE_FIELD_ALIAS not in v:
             v[Naming.TYPE_FIELD_ALIAS] = cls.discriminator()
         return v
